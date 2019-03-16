082ef06447697ab53f39e1b0708be2e0788681ac
jsjchai <jsjchai@163.com>
2019-03-16 22:34:15 +0800

    zookeeper

--- src/test/java/com/jsjchai/distributed/lock/zk/ZookeeperTest.java
+++ src/test/java/com/jsjchai/distributed/lock/zk/ZookeeperTest.java
@@ -5,6 +5,7 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -21,7 +22,6 @@ public class ZookeeperTest {
         try {
             ZooKeeper client = new ZooKeeper("127.0.0.1:2181",  10000,null);
             log.info("state:{}",client.getState().toString());
-            //client.create("/test",null, ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
             List<String> childs =  client.getChildren("/",null);
             if(childs == null || childs.isEmpty()){
                 log.info("childs is null");
@@ -36,4 +36,57 @@ public class ZookeeperTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void create(){
+        try {
+            ZooKeeper client = new ZooKeeper("127.0.0.1:2181",  10000,null);
+            String path = "/test";
+            Stat stat = client.exists(path,null);
+            if(stat != null){
+                log.info(path+"is exists");
+                return;
+            }
+            client.create(path,"test".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+            log.info(path +" create success");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        } catch (KeeperException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getData(){
+        try {
+            ZooKeeper client = new ZooKeeper("127.0.0.1:2181",  10000,null);
+            String path = "/test";
+
+            log.info("client get data:{}",new String(client.getData(path,null,null)));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        } catch (KeeperException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void del(){
+        try {
+            ZooKeeper client = new ZooKeeper("127.0.0.1:2181",  10000,null);
+            String path = "/test";
+            client.delete(path,0);
+            log.info("client del success");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        } catch (KeeperException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
